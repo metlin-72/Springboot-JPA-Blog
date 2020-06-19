a634257212ef5f8e15c79db8a4b131d2311b5957
이노가드_김원기 <metlin@inogard.co.kr>
2020-06-19 14:28:19 +0900

    공백 삭제
    Signed-off-by: 이노가드_김원기 <metlin@inogard.co.kr>

--- src/main/java/com/metlin/blog/config/SecurityConfig.java
+++ src/main/java/com/metlin/blog/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/auth/**")
+				.antMatchers("/auth/**", "/js/**", "/css/**", "/images/**")
 				.permitAll()
 				.anyRequest()
 				.authenticated()
