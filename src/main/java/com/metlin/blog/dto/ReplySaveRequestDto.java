19fe8cb49a2f3c4df641d8e83c3b37a26ae03a63
metlin <metlin@naver.com>
2020-07-30 22:36:39 +0900

    2020.07.30 집, 수정

--- src/main/java/com/metlin/blog/dto/ReplySaveRequestDto.java
+++ src/main/java/com/metlin/blog/dto/ReplySaveRequestDto.java
@@ -7,4 +7,13 @@ public class ReplySaveRequestDto {
     private int userId;
     private int boardId;
     private String content;
+
+    public ReplySaveRequestDto() {
+    }
+
+    public ReplySaveRequestDto(int userId, int boardId, String content) {
+        this.userId = userId;
+        this.boardId = boardId;
+        this.content = content;
+    }
 }
