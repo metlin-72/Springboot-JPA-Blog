ccd0773c18cf09a6b3d22b86db3d80e75ec03dd2
이노가드_김원기 <metlin@inogard.co.kr>
2020-08-26 10:40:41 +0900

    2020.08.26 Lombok 다시 적용

--- src/main/java/com/metlin/blog/dto/ReplySaveRequestDto.java
+++ src/main/java/com/metlin/blog/dto/ReplySaveRequestDto.java
@@ -1,19 +1,14 @@
 package com.metlin.blog.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReplySaveRequestDto {
     private int userId;
     private int boardId;
     private String content;
-
-    public ReplySaveRequestDto() {
-    }
-
-    public ReplySaveRequestDto(int userId, int boardId, String content) {
-        this.userId = userId;
-        this.boardId = boardId;
-        this.content = content;
-    }
 }
